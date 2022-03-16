4d0b72eeafc36aeedb85be46502f5356ca838c78
niama315 <aroubiteniama@gmail.com>
2022-03-16 10:47:01 +0100

    init

---
+++ src/main/java/com/ensitech/EnsitechApplication.java
@@ -0,0 +1,13 @@
+package com.ensitech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsitechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnsitechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ensitech/EnsitechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensitech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnsitechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
