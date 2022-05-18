cf201cad1a333d5fcda79b89177e3ce51bc49147
yassine315 <yassine.elmahi77@gmail.com>
2022-05-18 11:07:46 +0100

    delete etudiant

--- src/main/java/com/ensitech/cotroller/EtudiantController.java
+++ src/main/java/com/ensitech/cotroller/EtudiantController.java
@@ -6,6 +6,7 @@ import com.ensitech.repository.EtudierRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
@@ -25,5 +26,11 @@ public class EtudiantController {
         etudiantRepository.save(etudiant);
         return new RedirectView("/home");
 
+    }
+
+    @GetMapping("/delete")
+    RedirectView delete(@RequestParam(name = "id") Long id){
+        etudiantRepository.deleteById(id);
+        return new RedirectView("/home");
     }
 }
